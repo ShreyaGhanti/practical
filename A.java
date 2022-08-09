beeda55532184b5d89401b904581460c18127df7
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-09 17:09:41 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,19 @@
+class A
+{
+
+static int c=40;
+int a=10;
+public static void main(String[]args)
+{
+A obj=new A();
+
+int b=50;
+
+ System.out.println(A.c);
+ System.out.println(b);
+ System.out.println(obj.a);
+}
+}
+
+
+ 
