e0faf09e8395d24436501b6f626ef2db8c36f55b
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-09 18:19:17 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication{
+public static void main(String[]args)
+{
+System.out.println("The two number is:");
+int a=30;
+int b=10;
+System.out.println(a+" "+b);
+int multi=a*b;
+System.out.println("The multi is :" +multi);
+}
+}
