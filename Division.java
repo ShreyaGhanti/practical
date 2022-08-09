fa2306c80c4e6b27dce78b445e50b4e159774050
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-09 18:17:31 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,11 @@
+class Division{
+public static void main(String[]args)
+{
+System.out.println("The two number is:");
+int a=45;
+int b=5;
+System.out.println(a+" "+b);
+int divi=a/b;
+System.out.println("The divi is :" +divi);
+}
+}
