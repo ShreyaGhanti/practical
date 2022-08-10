ea2277d108486d94ad8cc7c0622c4d320e203c63
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-10 16:16:52 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle{
+public static void main(String[]args){
+int l=5,b=10;
+int perimeter=(2*(l+b));
+int area=(l*b) ;
+System.out.println("Perimeter of the Rectangle:="+perimeter);
+System.out.println("Area of the Rectangle:="+area);
+}
+}
