276fac4e1e8b4cbaf33e04a7fa4b8ffb6205c00b
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-10 16:17:54 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,9 @@
+public class Square{
+public static void main(String[]args){
+float s=6.8F;
+float area=(s*s);
+float perimeter=(4*s) ;
+System.out.println("Area of the Square:="+area);
+System.out.println("Perimeter of the Square:="+perimeter);
+}
+}
