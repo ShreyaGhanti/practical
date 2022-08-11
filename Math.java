7c39d3f150adf064a1eaaef0893e68cb1da561a7
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-11 15:46:44 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,18 @@
+class Math{
+public static void main(String[]args){
+System.out.println("the two numbers is:");
+int a=40;
+int b=20;
+int sum=a+b;
+int multi=a*b;
+int sub=a-b;
+int divi=a/b;
+System.out.println("40,20");
+System.out.println("Sum of two numbers:"+sum);
+System.out.println("Multi of two numbers:"+multi);
+System.out.println("Sub of two numbers:"+sub);
+System.out.println("Divi of two numbers:"+divi);
+}
+}
+
+
