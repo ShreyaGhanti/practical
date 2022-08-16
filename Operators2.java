a8963d291083af040f7b75e41dba7432bc993e61
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-17 01:40:29 +0530

    Add files via upload

---
+++ Operators2.java
@@ -0,0 +1,20 @@
+package operators1;
+
+public class Operators2 {
+
+	public static void main(String[] args) {
+		// initialize a&b
+		int a=10;
+		int b=20;
+		System.out.println(a+b);//addition
+		System.out.println(a-b);//subtraction
+		System.out.println(a/b);//division
+		System.out.println(a*b);//multipliclation
+		System.out.println(a%b);//mod
+		System.out.println(10*5/3+2-1*6/3);
+		System.out.println(a<b||b<a);//treu||false=true
+				
+
+	}
+
+}
