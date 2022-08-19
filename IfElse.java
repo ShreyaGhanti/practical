f904a2ae22f22c1c445bc495a4dd759a64cf4eeb
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-19 17:08:32 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+
+public class IfElse {
+
+		public static void main(String[] args) {
+			int x=5;
+			int y=3;
+			if(x+y<10) {
+			System.out.println("x+y is less then 10");
+			}
+			else
+			{
+		    System.out.println("x+y is greater than 10");
+			}
+
+		}
+
+	}
+
+
