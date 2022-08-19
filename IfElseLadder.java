a8507ab2e1bf0f9f2c71f509527fdaa29a6113ae
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-19 17:08:46 +0530

    Add files via upload

---
+++ IfElseLadder.java
@@ -0,0 +1,19 @@
+
+public class IfElseLadder {
+
+	public static void main(String[] args) {
+	String name ="Shreya";
+	if(name=="priya") {
+		System.out.println("name is priya");
+	}else if (name=="puja") {
+		System.out.println("name is puja");
+	}else if (name=="pritam") {
+		System.out.println("name is pritam");
+	}else {
+		System.out.println(name );
+	}
+	}
+	}
+
+
+
