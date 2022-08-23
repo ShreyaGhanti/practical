be5e5630b474924e4634e5e68708ee70ba96536c
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-23 17:21:50 +0530

    Add files via upload

---
+++ ForEachloop.java
@@ -0,0 +1,13 @@
+
+public class ForEachloop {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int arr[]= {1,2,3,4,5,6,7};
+		for(int i:arr){
+			System.out.println(i);
+			sum=sum+i;}
+System.out.println("sum="+sum);
+	}
+
+}
