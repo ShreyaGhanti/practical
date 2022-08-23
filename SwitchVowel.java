e07d8ce397d1f86f74a4ca124ba85fd3b2931040
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-23 16:47:13 +0530

    Add files via upload

---
+++ SwitchVowel.java
@@ -0,0 +1,19 @@
+
+public class SwitchVowel {
+
+	public static void main(String[] args) {
+		char oh ='e';
+		switch(oh) {
+		case 'y':System.out.println("its not a vowel");
+		break;
+		case 'e':System.out.println("its  a vowel");
+		break;
+		case 'g':System.out.println("its not a vowel");
+		break;
+		case 'm':System.out.println("its not a vowel");
+		break;
+		}
+
+	}
+
+}
