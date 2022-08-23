f1bf8046ea6734fb2da68b9909d6903d52c7f29d
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-23 16:46:44 +0530

    Add files via upload

---
+++ SwitchEg.java
@@ -0,0 +1,25 @@
+
+public class SwitchEg {
+
+	public static void main(String[] args) {
+	int day =6;
+	String dayString="";
+	switch (day){
+	case 1: dayString="1-sunday";
+	break;
+	case 2: dayString="2-monday";
+	break;
+	case 3: dayString="3-tuesday";
+	break;
+	case 4: dayString="4-wednesday";
+	break;
+	case 5: dayString="5-thursday";
+	break;
+	case 6: dayString="6-friday";
+	break;
+	case 7: dayString="7-saturday";
+	break;
+	default:System.out.println("invalid day");	
+	}
+		System.out.println(dayString);
+	}}
