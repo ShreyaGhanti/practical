b3392d7ddab9ea491ca66ee7a1867d4004e428ba
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-08-23 16:46:25 +0530

    Add files via upload

---
+++ SwitchTest.java
@@ -0,0 +1,22 @@
+package com;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		int num=40;
+		switch(num) {
+		case 1 :System.out.println("12");
+		break;
+		case 12 :System.out.println("15");
+		break;
+		case 16:System.out.println("13");
+		break;
+		case 40 :System.out.println("very good");
+		break;
+		default:System.out.println("number is not within the range");
+		
+		}
+
+	}
+
+}
