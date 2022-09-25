9346bbff053261e063cfa30cb43454d4ea15d4a9
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-09-26 00:22:15 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,18 @@
+public abstract class Abstract {//parent class
+abstract void method();// abstract method
+}
+class Test extends Abstract{//child class 
+	void method() // non abstract method
+	{
+		System.out.println(" method running successfully");
+	}
+}
+class NewTest extends Abstract{//child class 2
+	void method() {
+		System.out.println(" new test method running successfully");
+	}
+	public static void main(String[] args) {
+		Abstract abs=new NewTest();
+		abs.method();
+	}
+}
