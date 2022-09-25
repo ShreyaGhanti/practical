c6f348becd46c2bdad4ef61c2573a07ba114c77d
ShreyaGhanti <110472485+ShreyaGhanti@users.noreply.github.com>
2022-09-26 00:22:45 +0530

    Add files via upload

---
+++ Democlass.java
@@ -0,0 +1,20 @@
+Interface Printable{  
+void print();  
+}  
+interface Showable{  
+void print();//void show();  
+}  
+class DemoClass implements Printable,Showable{  
+public void print() {
+    System.out.println("Hello");
+}  
+/*public void show(){
+	System.out.println("Welcome");
+*/	
+  
+public static void main(String args[]){  
+	DemoClass obj = new DemoClass();  
+obj.print();  
+//obj.show();  
+ }  
+}
